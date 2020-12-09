a146cdf1ca296e6f5eb5173597137f236bc7f32f
Pedram Valiani <p.valiani@gmail.com>
2020-12-09 14:40:25 +0000

    second commit files all working

--- lab_callbacks_films_start/models/cinema.js
+++ lab_callbacks_films_start/models/cinema.js
@@ -2,4 +2,10 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+
+Cinema.prototype.filmTitles = function () {
+  const filmTitles = this.films.map(film => film.title);
+  return filmTitles;
+};
+
 module.exports = Cinema;
--- lab_callbacks_films_start/specs/cinema_spec.js
+++ lab_callbacks_films_start/specs/cinema_spec.js
@@ -28,12 +28,20 @@ describe('Cinema', function () {
     assert.deepStrictEqual(actual, films);
   });
 
-  it('should be able to get a list of film titles');
-  it('should be able to find a film by title');
-  it('should be able to filter films by genre');
-  it('should be able to check whether there are some films from a particular year');
-  it('should be able to check whether there are no films from a particular year');
-  it('should be able to check whether all films are over a particular length');
-  it('should be able to calculate total running time of all films');
+  xit('should be able to get a list of film titles', function() {
+
+  const actual = cinema.filmTitles();
+  assert.deepStrictEqual(actual, [moonlight.title, bladeRunner.title, dunkirk.title, blackPanther.title, trainspotting.title]);
+
+
+  });
+
+
+  xit('should be able to find a film by title');
+  xit('should be able to filter films by genre');
+  xit('should be able to check whether there are some films from a particular year');
+  xit('should be able to check whether there are no films from a particular year');
+  xit('should be able to check whether all films are over a particular length');
+  xit('should be able to calculate total running time of all films');
 
 });
